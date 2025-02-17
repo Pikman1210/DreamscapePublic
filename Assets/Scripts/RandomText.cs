41bb6b5f1750927256b69bf23fda1afb21323dba
Pikman1210 <72507796+Pikman1210@users.noreply.github.com>
2025-02-17 16:27:02 -0600

    Subtitle Randomization

---
+++ Assets/Scripts/RandomText.cs
@@ -0,0 +1,22 @@
+using QFSW.QC;
+using TMPro;
+using UnityEngine;
+
+public class RandomText : MonoBehaviour
+{
+    public TextObject textObject;
+    private TMP_Text subtitle;
+
+    private void Start()
+    {
+        subtitle = GetComponent<TMP_Text>();
+        subtitle.text = textObject.strings[Random.Range(0, textObject.strings.Length)];
+    }
+
+    [Command]
+    public void RandomizeText()
+    {
+        subtitle = GetComponent<TMP_Text>();
+        subtitle.text = textObject.strings[Random.Range(0, textObject.strings.Length)];
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/TextObject.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Text List", menuName = "TextList")]
+public class TextObject : ScriptableObject
+{
+
+    public string[] strings;
+
+}
