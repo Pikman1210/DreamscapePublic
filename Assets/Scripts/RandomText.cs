eea4544e2e918d431242d555f1428a7a6c414e6c
Pikman1210 <72507796+Pikman1210@users.noreply.github.com>
2025-02-18 17:59:54 -0600

    Random Subtitle School Mode

--- Assets/Scripts/RandomText.cs
+++ Assets/Scripts/RandomText.cs
@@ -7,11 +7,12 @@ public class RandomText : MonoBehaviour
     public TextObject textObject;
     private TMP_Text subtitle;
 
+    /*
     private void Start()
     {
         subtitle = GetComponent<TMP_Text>();
         subtitle.text = textObject.strings[Random.Range(0, textObject.strings.Length)];
-    }
+    } */
 
     [Command]
     public void RandomizeText()
